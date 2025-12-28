fb3ccc7bc0266846e25a6baee2f0fe01e6b254ca
livekora611-design <livekora611@gmail.com>
2025-12-28 23:37:23 +0200

    Update Matches.js

--- Matches.js
+++ Matches.js
@@ -1,11 +1,20 @@
-moviesData({
-  "title": "سينما الأفلام",
-  "matches": [
-    {
-      "name": "Night of the Living Dead (1968)",
-      "time": "01:36:00",
-      "poster": "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_itemimage.jpg",
-      "url": "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_512kb.mp4"
-    }
-  ]
-});
+var matches = [
+  {
+    "title": "Night of the Living Dead (1968)",
+    "image": "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_itemimage.jpg",
+    "duration": "01:36:00",
+    "url": "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_512kb.mp4"
+  },
+  {
+    "title": "Plan 9 from Outer Space (1959)",
+    "image": "https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959_itemimage.jpg",
+    "duration": "01:19:00",
+    "url": "https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959_512kb.mp4"
+  },
+  {
+    "title": "The Brain That Wouldn't Die (1962)",
+    "image": "https://archive.org/download/TheBrainThatWouldntDie/TheBrainThatWouldntDie_itemimage.jpg",
+    "duration": "01:22:00",
+    "url": "https://archive.org/download/TheBrainThatWouldntDie/TheBrainThatWouldntDie_512kb.mp4"
+  }
+];
