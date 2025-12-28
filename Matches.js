9d720df374bb5422aafa0a91c24b138b3d23e8b6
livekora611-design <livekora611@gmail.com>
2025-12-28 23:39:09 +0200

    Update Matches.js

--- Matches.js
+++ Matches.js
@@ -1,20 +1,14 @@
 var matches = [
   {
-    "title": "Night of the Living Dead (1968)",
-    "image": "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_itemimage.jpg",
-    "duration": "01:36:00",
-    "url": "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_512kb.mp4"
+    title: "Night of the Living Dead (1968)",
+    image: "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_itemimage.jpg",
+    duration: "01:36:00",
+    url: "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_512kb.mp4"
   },
   {
-    "title": "Plan 9 from Outer Space (1959)",
-    "image": "https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959_itemimage.jpg",
-    "duration": "01:19:00",
-    "url": "https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959_512kb.mp4"
-  },
-  {
-    "title": "The Brain That Wouldn't Die (1962)",
-    "image": "https://archive.org/download/TheBrainThatWouldntDie/TheBrainThatWouldntDie_itemimage.jpg",
-    "duration": "01:22:00",
-    "url": "https://archive.org/download/TheBrainThatWouldntDie/TheBrainThatWouldntDie_512kb.mp4"
+    title: "Plan 9 from Outer Space (1959)",
+    image: "https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959_itemimage.jpg",
+    duration: "01:19:00",
+    url: "https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959_512kb.mp4"
   }
 ];
