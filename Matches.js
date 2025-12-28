a2cefb8454821d51dec0d0eeb82ccb2d7be716e6
livekora611-design <livekora611@gmail.com>
2025-12-28 23:20:23 +0200

    Update Matches.js

--- Matches.js
+++ Matches.js
@@ -1,23 +1,16 @@
 moviesData({
-  "title": "سينما الأفلام",
   "matches": [
     {
-      "name": "Night of the Living Dead (1968)",
-      "time": "01:36:00",
-      "poster": "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_itemimage.jpg",
-      "url": "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead_512kb.mp4"
+      "name": "فيلم الأكشن 1",
+      "time": "1:45",
+      "poster": "https://i.imgur.com/8RKXAIV.jpg",
+      "url": "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
     },
     {
-      "name": "Plan 9 from Outer Space (1959)",
-      "time": "01:19:00",
-      "poster": "https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959_itemimage.jpg",
-      "url": "https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959_512kb.mp4"
-    },
-    {
-      "name": "The Brain That Wouldn't Die (1962)",
-      "time": "01:22:00",
-      "poster": "https://archive.org/download/TheBrainThatWouldntDie/TheBrainThatWouldntDie_itemimage.jpg",
-      "url": "https://archive.org/download/TheBrainThatWouldntDie/TheBrainThatWouldntDie_512kb.mp4"
+      "name": "فيلم الرعب 2",
+      "time": "2:10",
+      "poster": "https://i.imgur.com/J5LVHEL.jpg",
+      "url": "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
     }
   ]
 });
